ccaf8305f3e04af6281532aa17d5fcde5e4f3d49
jtb <bloo1987@proton.me>
2024-04-08 09:04:15 -0700

    decoupled notification error codes and subcodes + test updates

--- src/errors.rs
+++ src/errors.rs
@@ -19,51 +19,25 @@ impl Error for SubcodeError {}
 
 #[derive(Debug, PartialEq)]
 pub(crate) enum NotifErrorCode {
-    MessageHeaderError(u8, NotifErrorSubCode),
-    OpenMessageError(u8, NotifErrorSubCode),
-    UpdateMessageError(u8, NotifErrorSubCode),
+    MessageHeaderError(u8),
+    OpenMessageError(u8),
+    UpdateMessageError(u8),
     HoldTimerExpired(u8),
     FiniteStateMachineError(u8),
     Cease(u8),
 }
-#[derive(Debug, PartialEq)]
-pub(crate) enum NotifErrorSubCode {
-    MessageHeaderError(u8),
-    OpenMessageError(u8),
-    UpdateMessageError(u8),
-}
+
 
 impl NotifErrorCode {
     // To get the default variants, will use these generating functions.
-    pub(crate) fn msg_header_err(subcode: u8) -> Result<Self, SubcodeError> {
-        match subcode {
-            1 | 2 | 3 => {
-                Ok(Self::MessageHeaderError(1, NotifErrorSubCode::MessageHeaderError(subcode)))
-            }
-            _ => {
-                Err(SubcodeError(String::from("Valid values are 1, 2, or 3.")))
-            }
-        }
+    pub(crate) fn msg_header_err(subcode: u8) -> Self {
+        Self::MessageHeaderError(1)
     }
-    pub(crate) fn open_msg_err(subcode: u8) -> Result<Self, SubcodeError> {
-        match subcode {
-            1..=6 => {
-                Ok(Self::OpenMessageError(2, NotifErrorSubCode::OpenMessageError(subcode)))
-            }
-            _ => {
-                Err(SubcodeError(String::from("Valid values are integers 1 through 6.")))
-            }
-        }
+    pub(crate) fn open_msg_err(subcode: u8) -> Self {
+        Self::OpenMessageError(2)
     }
-    pub(crate) fn update_msg_err(subcode: u8) -> Result<Self, SubcodeError> {
-        match subcode {
-            1..=11 => {
-                Ok(Self::UpdateMessageError(3, NotifErrorSubCode::UpdateMessageError(subcode)))
-            }
-            _ => {
-                Err(SubcodeError(String::from("Valid values are integers 1 through 11.")))
-            }
-        }
+    pub(crate) fn update_msg_err() -> Self {
+         Self::UpdateMessageError(3)       
     }
     pub(crate) fn hold_timer_exp() -> Self {
         Self::HoldTimerExpired(4)
@@ -76,24 +50,65 @@ impl NotifErrorCode {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub(crate) enum NotifErrorSubCode {
+    MessageHeaderError(u8),
+    OpenMessageError(u8),
+    UpdateMessageError(u8),
+}
+impl NotifErrorSubCode {
+    pub(crate) fn msg_header_err(subcode: u8) -> Result<Self, SubcodeError> {
+        match subcode {
+            1 | 2 | 3 => {
+                Ok(Self::MessageHeaderError(subcode))
+            }
+            _ => {
+                Err(SubcodeError(String::from("Valid values are 1, 2, or 3.")))
+            }
+        }
+    }
+    pub(crate) fn open_msg_err(subcode: u8) -> Result<Self, SubcodeError> {
+        match subcode {
+            1..=6 => {
+                Ok(Self::OpenMessageError(subcode))
+            }
+            _ => {
+                Err(SubcodeError(String::from("Valid values are integers 1 through 6.")))
+            }
+        }
+    }
+    pub(crate) fn update_msg_err(subcode: u8) -> Result<Self, SubcodeError> {
+        match subcode {
+            1..=11 => {
+                Ok(Self::UpdateMessageError(subcode))
+            }
+            _ => {
+                Err(SubcodeError(String::from("Valid values are integers 1 through 11.")))
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn build_default_msg_header_err_valid_subcode() {
-        for i in 1u8..3 {
-            if let Ok(err) = NotifErrorCode::msg_header_err(i) {
-                assert_eq!(err, NotifErrorCode::MessageHeaderError(1, NotifErrorSubCode::MessageHeaderError(i)));
-            } else {
-                panic!("Expected values 1 through 3 to result in an Ok(), got Err()!")
+    fn build_default_valid_subcode_msg_header_err() {
+        for i in 1u8..=3 {
+            match NotifErrorSubCode::msg_header_err(i) {
+                Ok(err) => {
+                    assert_eq!(err, NotifErrorSubCode::MessageHeaderError(i));
+                }
+                _ => {
+                    panic!("Expected values 1 through 3 to result in an Ok(), got Err()!")
+                }
             }
         }
     }
     #[test]
-    fn build_default_msg_header_err_invalid_subcode() {
-        let err = NotifErrorCode::msg_header_err(5);
-        match err {
+    fn build_default_invalid_subcode_msg_header_err() {
+        match NotifErrorSubCode::msg_header_err(5) {
             Err(e) => {
               assert_eq!(e, SubcodeError(String::from("Valid values are 1, 2, or 3.")))
             }
@@ -103,19 +118,21 @@ mod tests {
         }
     }
     #[test]
-    fn build_default_open_msg_err_valid_subcode() {
-        for i in 1u8..6 {
-            if let Ok(err) = NotifErrorCode::open_msg_err(i) {
-                assert_eq!(err, NotifErrorCode::OpenMessageError(2, NotifErrorSubCode::OpenMessageError(i)));
-            } else {
-                panic!("Expected values 1 through 6 to result in an Ok(), got Err()!")
+    fn build_default_valid_subcode_open_msg_err() {
+        for i in 1u8..=6 {
+            match NotifErrorSubCode::open_msg_err(i) {
+                Ok(err) => {
+                    assert_eq!(err, NotifErrorSubCode::OpenMessageError(i));
+                }
+                _ => {
+                    panic!("Expected values 1 through 6 to result in an Ok(), got Err()!")
+                }
             }
         }
     }
     #[test]
-    fn build_default_open_msg_err_invalid_subcode() {
-        let err = NotifErrorCode::open_msg_err(7);
-        match err {
+    fn build_default_invalid_subcode_open_msg_err() {
+        match NotifErrorSubCode::open_msg_err(7) {
             Err(e) => {
               assert_eq!(e, SubcodeError(String::from("Valid values are integers 1 through 6.")))
             }
@@ -125,19 +142,21 @@ mod tests {
         }
     }
     #[test]
-    fn build_default_update_msg_err_valid_subcode() {
+    fn build_default_valid_subcode_update_msg_err() {
         for i in 1u8..=11 {
-            if let Ok(err) = NotifErrorCode::update_msg_err(i) {
-                assert_eq!(err, NotifErrorCode::UpdateMessageError(3, NotifErrorSubCode::UpdateMessageError(i)));
-            } else {
-                panic!("Expected values 1 through 11 to result in an Ok(), got Err()!")
+            match NotifErrorSubCode::update_msg_err(i) {
+                Ok(err) => {
+                    assert_eq!(err, NotifErrorSubCode::UpdateMessageError(i));
+                }
+                _ => {
+                    panic!("Expected values 1 through 11 to result in an Ok(), got Err()!")
+                }
             }
         }
     }
     #[test]
-    fn build_default_update_msg_err_invalid_subcode() {
-        let err = NotifErrorCode::update_msg_err(12);
-        match err {
+    fn build_default_invalid_subcode_update_msg_err() {
+        match NotifErrorSubCode::update_msg_err(12) {
             Err(e) => {
               assert_eq!(e, SubcodeError(String::from("Valid values are integers 1 through 11.")))
             }
@@ -146,7 +165,6 @@ mod tests {
             }
         }
     }
-
     #[test]
     fn build_default_hold_timer_exp() {
         let err = NotifErrorCode::hold_timer_exp();
