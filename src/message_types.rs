b4a7b6f4f7438b2b2c8c9c2dd5233497d1a7ece0
jtb <bloo1987@proton.me>
2024-04-27 14:47:06 -0700

    updated notification type API + tests

--- src/message_types.rs
+++ src/message_types.rs
@@ -1,5 +1,13 @@
-use std::{cell::RefCell}; // For testing private structs
-use crate::errors;
+use std::{
+    cell::RefCell,
+    convert::From,
+};
+use crate::errors::{
+    NotifErrorCode,
+    OpenMsgErrSubcode,
+    UpdateMsgErrSubcode,
+    MsgHeaderErrSubcode,
+};
 use crate::path_attrs;
 
 // Definitions for the basic message types in BGP.
@@ -12,15 +20,15 @@ type KeepAlive = Header;
 
 #[derive(Debug)]
 pub struct Header {
-     marker: [u8; 16],
+     pub marker: [u8; 16],
     // Limited to 16 bits
-     length: usize,
+     pub length: u16,
     // Type; limited to 8 bits
-     message_type: u8,
+     pub message_type: u8,
 }
 
 impl Header {
-    pub fn new(length: usize, message_type: MessageType) -> Self {
+    pub fn new(length: u16, message_type: MessageType) -> Self {
         // First need to actually get the bit values from the MessageType
         let mtype = match message_type {
             MessageType::Open => OPEN_VALUE,
@@ -41,7 +49,7 @@ pub enum MessageType {
     KeepAlive,
     Notification
 }
-struct Open {
+pub (crate) struct Open {
     version: u8,
     // "My Autonomous System"
     my_as: u16,
@@ -55,7 +63,7 @@ struct Open {
     // are inhomogenous in length.
     opt_params: Vec<Tlv>,
 }
-struct Update {
+pub (crate) struct Update {
     withdrawn_routes_len: u16,
     withdrawn_routes: Vec<Route>,
     total_path_attr_len: u16,
@@ -65,16 +73,43 @@ struct Update {
     // routes only need prefix info to be removed.
     nlri: Vec<Route>,
 }
-struct Notification {
-    // "Error Code". This will get serialized to a u8
-    err_code: errors::NotifErrorCode,
-    // "Error Subcode". This will get serialized to a u8. 
-    // need to decouple NotifErrorCode and Subcode in errors mod.
-    err_subcode: errors::NotifErrorSubCode,
-    // "Data"; variable length. There is no length field for this since
+pub(crate) struct Notification {
+    // Notification Error Code
+    err_code: u8,
+    // Error Subcode
+    err_subcode: u8, 
+    // Data; variable length. There is no length field for this since
     // the length can be dynamically determined since each structure in the
     // message has a known length.
     data: Vec<u8>
+}
+
+impl Notification {
+    pub fn new(error: NotifErrorCode, data: usize) -> Self {
+        // Extract the error code and subcode from the NotifErrorCode instance
+        let err_code: u8 = error.as_ref().into();
+        let err_subcode: u8 = match error.as_ref() {
+            NotifErrorCode::MessageHeaderError(inner) => inner.into(),
+            NotifErrorCode::OpenMessageError(inner) => inner.into(),
+            NotifErrorCode::UpdateMessageError(inner) => inner.into(),
+            // RFC 4271, Pg.21; Error codes without defined subcodes should use 0 as subcode
+            _ => 0
+        };
+        Self {
+            err_code,
+            err_subcode,
+            data: Vec::from(data.to_be_bytes())
+        }
+    }
+    pub fn err_code(&self) -> u8 {
+        self.err_code
+    }
+    pub fn err_subcode(&self) -> u8 {
+        self.err_subcode
+    }
+    pub fn data(&self) -> &[u8] {
+        self.data.as_ref()
+    }
 }
 struct Tlv { // These will be constructed on the fly
     param_type: u8,
@@ -151,5 +186,27 @@ mod tests {
         let cell = RefCell::new(tlv);
         assert_eq!(cell.borrow().param_length, 2);
         assert_eq!(cell.borrow().param_type, 2);
+    }
+    #[test]
+    fn build_notification_with_subcode() {
+        let err_code = NotifErrorCode::OpenMessageError(OpenMsgErrSubcode::BadBgpId);
+        let msg = Notification::new(err_code, 1);
+        assert_eq!(msg.err_code(), 2);
+        assert_eq!(msg.err_subcode(), 3);
+
+        let mut data: [u8; 8] = [0; 8];
+        data.copy_from_slice(msg.data());
+        assert_eq!(usize::from_be_bytes(data), 1);
+    }
+    #[test]
+    fn build_notification_no_subcode() {
+        let err_code = NotifErrorCode::Cease;
+        let msg = Notification::new(err_code, 1);
+        assert_eq!(msg.err_code(), 6);
+        assert_eq!(msg.err_subcode(), 0);
+
+        let mut data: [u8; 8] = [0; 8];
+        data.copy_from_slice(msg.data());
+        assert_eq!(usize::from_be_bytes(data), 1);
     }
 }
