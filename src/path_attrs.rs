e9c4f8f03387d1ed09845ae23c7a0c151dacb789
jtb <bloo1987@proton.me>
2024-04-29 16:46:29 -0700

    simplified rework + impls for Origin and AS_PATH + tests

--- src/path_attrs.rs
+++ src/path_attrs.rs
@@ -5,7 +5,7 @@
 // selectively serialize based off the State.
 
 use std::{
-    cell::RefCell, error::Error, fmt::Display, mem::size_of, net::{IpAddr, Ipv4Addr, Ipv6Addr}
+    cell::RefCell, error::Error, fmt::Display, marker::PhantomData, mem::size_of, net::{IpAddr, Ipv4Addr, Ipv6Addr}
 };
 
 use crate::message_types::{
@@ -26,7 +26,7 @@ impl Error for PathAttrError {}
 // This Trait is necessary since the size of the data field for eath PA
 // is variable. Will be used for creating a trait object for use in containers.
 // Also making this implement ByteLen for easing the building of Update messages.
-pub(crate) trait PAttr: ByteLen {
+pub(crate) trait PAttr {
     // Bit Fiddling
 
     fn set_opt_bit(&mut self) {
@@ -178,23 +178,26 @@ pub(crate) trait PAttr: ByteLen {
 //    }
 //}
 
-// Implement the base PathAttr Type, along with its States (marker structs).
-// The marker structs are used to flag whether the PA is Standard or Extended.
-pub(crate) struct PaStd;
-pub(crate) struct PaExt;
-pub(crate) struct PathAttr<T> {
-    state: T,
+// Enum to flag whether a PA is Standard or Extended
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) enum PathAttrLen {
+    Std(u8),
+    Ext(u16),
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct PathAttr {
     // Attribute Flags
     attr_flags: u8,
     // Attribute Type Code
     attr_type_code: u8,
     // Attribute Length; All PAs will have a u16 for the length.
-    // the state will determine how this is parsed and serialized (as u8 or u16)
-    attr_len: u16,
+    attr_len: PathAttrLen,
     attr_value: Vec<u8>,
 }
+
 // Both states will implement these methods the same
-impl<T> PAttr for PathAttr<T> {
+impl PAttr for PathAttr {
     fn set_opt_bit(&mut self) {
         // Set MSB (network byte order) to 1
         self.attr_flags = self.attr_flags | 1 << 7;
@@ -208,8 +211,18 @@ impl<T> PAttr for PathAttr<T> {
         self.attr_flags = self.attr_flags | 1 << 5;       
     }
 }
-// Both states will implement these methods the same.
-impl<T> PathAttr<T> {
+impl PathAttr {
+    pub fn new(
+        attr_type_code: u8,
+        attr_len: PathAttrLen,
+        attr_value: Vec<u8>) -> Self {
+            Self {
+                attr_flags: 0,
+                attr_type_code,
+                attr_len,
+                attr_value
+            }
+    }
     pub fn attr_type_code(&self) -> u8 {
         self.attr_type_code
     }
@@ -220,90 +233,78 @@ impl<T> PathAttr<T> {
         self.attr_value.as_slice()
     }
 }
-// Will have unique new() methods for PaExt and PaStd impls
-// since we want to restrict the user to only using u8's for
-// the attr_len for PaStd and u16's for PaExt. Will also use
-// custom attr_len getters for each type to enforce this on 
-// read.
-impl<PaExt> PathAttr<PaExt> {
-    pub fn new(attr_type_code: u8,
-               attr_len: u16,
-               attr_value: &[u8]) -> Self {
-            
-        Self { 
-               state: PaExt,
-               // Setting the extended bit
-               attr_flags: 0 | 1 << 4,
-               attr_type_code,
-               attr_len,
-               attr_value
-             }
-    }
-    pub fn attr_len(&self) -> u16 {
-        self.attr_len
+pub(crate) struct PathAttrBuilder<T> {
+    _marker: PhantomData<T>,
+    attr_value: Vec<u8>,
+}
+
+// This is a generic builder that can be used over any custom Path Attribute type.
+// May add a trait bound later that requires that requires each impl to have a build()
+// method.
+impl<T> PathAttrBuilder<T> {
+    pub fn new() -> Self {
+        Self {
+            _marker: PhantomData,
+            attr_value: Vec::new()
+        }
     }
 }
 
-impl<PaStd> PathAttr<PaStd> {
-    pub fn new(attr_type_code: u8,
-               attr_len: u8,
-               attr_value: &[u8]) -> Self {
-            
-        Self { 
-               state: PaStd,
-               // Unsetting the extended bit
-               attr_flags: 0,
-               attr_type_code,
-               attr_len: attr_len as u16,
-               attr_value
-             }
-    }
-    pub fn attr_len(&self) -> u8 {
-        self.attr_len as u8
-    }
-}
+// ** Individual Path Attribute Definitions **
 
-pub(crate) trait PaBuilder<T> {
-    fn build() -> PathAttr<T>;
-}
-
+// ** ORIGIN **
 pub(crate) struct Origin;
-impl PaBuilder<PaStd> for Origin {
-    fn build(mut self, origin_val: u8) -> PathAttr<PaStd> {
-        let mut pa: PathAttr<PaStd> = PathAttr::new(
+
+#[derive(Debug, Clone)]
+pub(crate) enum OriginValue {
+    Igp,
+    Egp,
+    Incomplete
+}
+
+impl From<OriginValue> for u8 {
+    fn from(value: OriginValue) -> Self {
+        match value {
+            OriginValue::Igp => 0,
+            OriginValue::Egp => 1,
+            OriginValue::Incomplete => 2
+        }   
+    }
+}
+
+impl PathAttrBuilder<Origin> {
+    pub fn origin(mut self, val: OriginValue) -> Self {
+        self.attr_value.push(val.into());
+        self
+    }
+    pub fn build(self) -> PathAttr {
+        let mut pa = PathAttr::new(
             1,
-            1,
-            vec![origin_val].as_slice(),
+            PathAttrLen::Std(1),
+            self.attr_value
         );
         pa.set_trans_bit();
         pa
     }
 }
 
-pub(crate) struct AsPath {
-    attr_value: Vec<u8>
-}
+// ** AS_PATH
+
+pub(crate) struct AsPath;
 enum AsSegment {
     // Used when building the AS_PATH PA. RFC 4721, Pg. 18
     // The vec holds ASes.
     AsSequence(Vec<u16>),
     AsSet(Vec<u16>)
 }
-impl ByteLen for AsSegment {
-    fn byte_len(&self) -> usize {
-        match self {
-            AsSegment::AsSequence(v) => size_of::<u16>() * v.len(),
-            AsSegment::AsSet(v) => size_of::<u16>() * v.len()
-        }
-    }
-}
-impl PaBuilder<PaStd> for AsPath {
-    fn build(&mut self, as_segs: Vec<AsSegment>) -> PathAttr<PaStd> {
+
+impl PathAttrBuilder<AsPath> {
+    pub fn as_segments(mut self, val: Vec<AsSegment>) -> Self {
         // Need to decompose the Vec<AsSegments> into a Vec<u8> to conform
         // to standard and store in local vec.
         // TO-DO: Try to use functional style here
         self.attr_value = Vec::new();
-        for seg in as_segs {
+        for seg in val {
             match seg {
                 AsSegment::AsSequence(ases) => {
                     // AS_SEQUENCE segment type is 2
@@ -325,162 +326,145 @@ impl PaBuilder<PaStd> for AsPath {
                 }
             }
         }
-        // Now can build the PA and return
-        let mut pa: PathAttr<PaStd> = PathAttr::new(
+        self
+    }
+
+    pub fn build(mut self) -> PathAttr {
+        let mut pa = PathAttr::new(
             2,
-            self.attr_value.len(),
-            self.attr_value.as_slice());
+            PathAttrLen::Std(self.attr_value.len() as u8),
+            self.attr_value
+        );
+        pa.set_trans_bit();
         pa
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
     use super::*;
 
     #[test]
-    fn build_origin_valid_value() {
-        for i in 0..=2 {
-            let origin = PathAttr::build_origin(i);
-            let cell = RefCell::new(origin);
-            match cell.borrow().as_ref() {
-                Ok(origin) => {
-                    // Only transitive bit should be set since this is a well-known, mandatory (non-optional)
-                    // PA. This means the attr flags field should equal 64 in decimal.
-                    assert_eq!(64, origin.attr_flags);
-                    assert_eq!(1, origin.attr_type_code);
-                    assert_eq!(i, origin.attr_value[0]);
-                }
-                _ => {
-                    println!("Expected Ok() for the given values, received and Err()")
-                }
-
-            };
+    fn build_origin() {
+        let variants = vec![OriginValue::Igp, OriginValue::Egp, OriginValue::Incomplete];
+        for (idx, v) in variants.into_iter().enumerate() {
+            let origin = PathAttrBuilder::<Origin>::new().origin(v).build();
+            assert_eq!(64, origin.attr_flags);
+            assert_eq!(1, origin.attr_type_code);
+            assert_eq!(PathAttrLen::Std(1), origin.attr_len);
+            assert_eq!(idx as u8, origin.attr_value[0]);
         }
     }
-    #[test]
-    fn build_origin_invalid_value() {
-        let origin = PathAttr::build_origin(11);
-        let cell = RefCell::new(origin);
-        match cell.borrow().as_ref() {
-            Ok(_) => {
-                panic!("Expected Err() due to incorrect value, got Ok()!")
-            }
-            Err(e) => {
-                assert_eq!(*e, PathAttrError(String::from("Invalid value, valid values are 0, 1, 2.")));
-            }
-        };
-    }
+
     #[test]
     fn build_as_path() {
         // Create a sequence of AS Segments. One AS_SET and one AS_SEQUENCE
         let as_segs = vec![AsSegment::AsSet(vec![65000u16, 65001]), AsSegment::AsSequence(vec![131u16, 30437])];
-        let aspath = PathAttr::build_as_path(as_segs);
-        let cell = RefCell::new(aspath);
+        let aspath = PathAttrBuilder::<AsPath>::new().as_segments(as_segs).build();
 
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 64);
-        assert_eq!(cell.borrow().attr_type_code, 2);
-        assert_eq!(cell.borrow().attr_len, 12);
         // Verify the path attr values are correctly encoded.
-        assert_eq!(cell.borrow().attr_value[0], 1); // AS_SET Segment type
-        assert_eq!(cell.borrow().attr_value[1], 2); // num ASes in AS_SET
-        assert_eq!(cell.borrow().attr_value[2], 253); // MSB of first AS
-        assert_eq!(cell.borrow().attr_value[3], 232); // LSB of first AS
-        assert_eq!(cell.borrow().attr_value[4], 253); // MSB of second AS
-        assert_eq!(cell.borrow().attr_value[5], 233); // LSB of second AS
-        assert_eq!(cell.borrow().attr_value[6], 2); // AS_SEQUENCE Segment type
-        assert_eq!(cell.borrow().attr_value[7], 2); // num ASes in AS_SEQUENCE
-        assert_eq!(cell.borrow().attr_value[8], 0); // MSB of first AS
-        assert_eq!(cell.borrow().attr_value[9], 131); // LSB of first AS
-        assert_eq!(cell.borrow().attr_value[10], 118); // MSB of second AS
-        assert_eq!(cell.borrow().attr_value[11], 229); // LSB of second AS
-    }
-    #[test]
-    fn build_next_hop_v4() {
-        let ip = Ipv4Addr::from_str("192.168.0.0").unwrap();
-        let n_hop = PathAttr::build_next_hop(IpAddr::V4(ip));
-        let cell = RefCell::new(n_hop);
-
         // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 64u8);
-        assert_eq!(cell.borrow().attr_type_code, 3u8);
-        assert_eq!(cell.borrow().attr_len, 4u8);
-        let mut bytes = [0u8; 4];
-        bytes.copy_from_slice(cell.borrow().attr_value.as_slice());
-        assert_eq!(Ipv4Addr::from(bytes), Ipv4Addr::from_str("192.168.0.0").unwrap());
-    }
-    #[test]
-    fn build_next_hop_v6() {
-        // Using Ipv6 Neighbor Solicitation dest address (multicast) because why not?
-        let ip = Ipv6Addr::new(0xFF02, 0, 0, 0, 0, 0x0001, 0xFFCC, 0xCCCC);
-        let n_hop = PathAttr::build_next_hop(IpAddr::V6(ip));
-        let cell = RefCell::new(n_hop);
-
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 64u8);
-        assert_eq!(cell.borrow().attr_type_code, 3u8);
-        assert_eq!(cell.borrow().attr_len, 16u8);
-
-        // Cumbersome to build an Ipv6Addr, so will just compare the octets.
-        assert_eq!(cell.borrow().attr_value, ip.octets());
-    }
-    #[test]
-    fn build_med() {
-        let med = PathAttr::build_med(1000);
-        let cell = RefCell::new(med);
-
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 128);
-        assert_eq!(cell.borrow().attr_type_code, 4);
-        assert_eq!(cell.borrow().attr_len, 4);
-        // Value check. Should be 1000 decomposed as a u8
-        assert_eq!(cell.borrow().attr_value, vec![0u8, 0, 3, 232]);
-    }
-    #[test]
-    fn build_local_pref() {
-        let lp = PathAttr::build_local_pref(1000);
-        let cell = RefCell::new(lp);
-
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 64);
-        assert_eq!(cell.borrow().attr_type_code, 5);
-        assert_eq!(cell.borrow().attr_len, 4);
-        // Value check. Should be 1000 decomposed as a u8
-        assert_eq!(cell.borrow().attr_value, vec![0u8, 0, 3, 232]);
-    }
-    #[test]
-    fn build_atomic_agg() {
-        let aa = PathAttr::build_atomic_agg();
-        let cell = RefCell::new(aa);
-
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 64);
-        assert_eq!(cell.borrow().attr_type_code, 6);
-        assert_eq!(cell.borrow().attr_len, 0);
-        // Value check. Should be 1000 decomposed as a u8
-        assert_eq!(cell.borrow().attr_value.is_empty(), true);
-        assert_eq!(cell.borrow().attr_value.len(), 0);
-    }
-    #[test]
-    fn build_aggregator_v4() {
-        let ag = PathAttr::build_aggregator_v4(65000, Ipv4Addr::new(1, 1, 1, 1));
-        let cell = RefCell::new(ag);
-
-        // Path Attr checks
-        assert_eq!(cell.borrow().attr_flags, 192);
-        assert_eq!(cell.borrow().attr_type_code, 7);
-        assert_eq!(cell.borrow().attr_len, 6);
-        
-        // First get the appropriate bytes from the vec as u8 arrays
-        let mut last_as_bytes: [u8; 2] = [0u8; 2];
-        let mut ip_bytes: [u8; 4] = [0u8; 4];
-        last_as_bytes.copy_from_slice(cell.borrow().attr_value[0..=1].as_ref());
-        ip_bytes.copy_from_slice(cell.borrow().attr_value[2..=5].as_ref());
-
-        // Now can check to see if they are correct.
-        assert_eq!(u16::from_be_bytes(last_as_bytes), 65000u16);
-        assert_eq!(Ipv4Addr::from(ip_bytes), Ipv4Addr::new(1, 1, 1, 1));
+        assert_eq!(aspath.attr_flags, 64);
+        assert_eq!(aspath.attr_type_code, 2);
+        assert_eq!(aspath.attr_len, PathAttrLen::Std(12));
+        assert_eq!(aspath.attr_value[0], 1); // AS_SET Segment type
+        assert_eq!(aspath.attr_value[1], 2); // num ASes in AS_SET
+        assert_eq!(aspath.attr_value[2], 253); // MSB of first AS
+        assert_eq!(aspath.attr_value[3], 232); // LSB of first AS
+        assert_eq!(aspath.attr_value[4], 253); // MSB of second AS
+        assert_eq!(aspath.attr_value[5], 233); // LSB of second AS
+        assert_eq!(aspath.attr_value[6], 2); // AS_SEQUENCE Segment type
+        assert_eq!(aspath.attr_value[7], 2); // num ASes in AS_SEQUENCE
+        assert_eq!(aspath.attr_value[8], 0); // MSB of first AS
+        assert_eq!(aspath.attr_value[9], 131); // LSB of first AS
+        assert_eq!(aspath.attr_value[10], 118); // MSB of second AS
+        assert_eq!(aspath.attr_value[11], 229); // LSB of second AS
     }
 }
+//    #[test]
+//    fn build_next_hop_v4() {
+//        let ip = Ipv4Addr::from_str("192.168.0.0").unwrap();
+//        let n_hop = PathAttr::build_next_hop(IpAddr::V4(ip));
+//        let cell = RefCell::new(n_hop);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 64u8);
+//        assert_eq!(cell.borrow().attr_type_code, 3u8);
+//        assert_eq!(cell.borrow().attr_len, 4u8);
+//        let mut bytes = [0u8; 4];
+//        bytes.copy_from_slice(cell.borrow().attr_value.as_slice());
+//        assert_eq!(Ipv4Addr::from(bytes), Ipv4Addr::from_str("192.168.0.0").unwrap());
+//    }
+//    #[test]
+//    fn build_next_hop_v6() {
+//        // Using Ipv6 Neighbor Solicitation dest address (multicast) because why not?
+//        let ip = Ipv6Addr::new(0xFF02, 0, 0, 0, 0, 0x0001, 0xFFCC, 0xCCCC);
+//        let n_hop = PathAttr::build_next_hop(IpAddr::V6(ip));
+//        let cell = RefCell::new(n_hop);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 64u8);
+//        assert_eq!(cell.borrow().attr_type_code, 3u8);
+//        assert_eq!(cell.borrow().attr_len, 16u8);
+//
+//        // Cumbersome to build an Ipv6Addr, so will just compare the octets.
+//        assert_eq!(cell.borrow().attr_value, ip.octets());
+//    }
+//    #[test]
+//    fn build_med() {
+//        let med = PathAttr::build_med(1000);
+//        let cell = RefCell::new(med);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 128);
+//        assert_eq!(cell.borrow().attr_type_code, 4);
+//        assert_eq!(cell.borrow().attr_len, 4);
+//        // Value check. Should be 1000 decomposed as a u8
+//        assert_eq!(cell.borrow().attr_value, vec![0u8, 0, 3, 232]);
+//    }
+//    #[test]
+//    fn build_local_pref() {
+//        let lp = PathAttr::build_local_pref(1000);
+//        let cell = RefCell::new(lp);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 64);
+//        assert_eq!(cell.borrow().attr_type_code, 5);
+//        assert_eq!(cell.borrow().attr_len, 4);
+//        // Value check. Should be 1000 decomposed as a u8
+//        assert_eq!(cell.borrow().attr_value, vec![0u8, 0, 3, 232]);
+//    }
+//    #[test]
+//    fn build_atomic_agg() {
+//        let aa = PathAttr::build_atomic_agg();
+//        let cell = RefCell::new(aa);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 64);
+//        assert_eq!(cell.borrow().attr_type_code, 6);
+//        assert_eq!(cell.borrow().attr_len, 0);
+//        // Value check. Should be 1000 decomposed as a u8
+//        assert_eq!(cell.borrow().attr_value.is_empty(), true);
+//        assert_eq!(cell.borrow().attr_value.len(), 0);
+//    }
+//    #[test]
+//    fn build_aggregator_v4() {
+//        let ag = PathAttr::build_aggregator_v4(65000, Ipv4Addr::new(1, 1, 1, 1));
+//        let cell = RefCell::new(ag);
+//
+//        // Path Attr checks
+//        assert_eq!(cell.borrow().attr_flags, 192);
+//        assert_eq!(cell.borrow().attr_type_code, 7);
+//        assert_eq!(cell.borrow().attr_len, 6);
+//        
+//        // First get the appropriate bytes from the vec as u8 arrays
+//        let mut last_as_bytes: [u8; 2] = [0u8; 2];
+//        let mut ip_bytes: [u8; 4] = [0u8; 4];
+//        last_as_bytes.copy_from_slice(cell.borrow().attr_value[0..=1].as_ref());
+//        ip_bytes.copy_from_slice(cell.borrow().attr_value[2..=5].as_ref());
+//
+//        // Now can check to see if they are correct.
+//        assert_eq!(u16::from_be_bytes(last_as_bytes), 65000u16);
+//        assert_eq!(Ipv4Addr::from(ip_bytes), Ipv4Addr::new(1, 1, 1, 1));
+//    }
+//}
