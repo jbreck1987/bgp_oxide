00cc4353499d83eec2ea15c900a62eceb60a99a2
jtb <bloo1987@proton.me>
2024-05-05 19:28:59 -0700

    reworked cmp for DecisionData + tests

--- src/table.rs
+++ src/table.rs
@@ -1,8 +1,12 @@
 // Holds logic for the BGP RIBs and Decision Process
 
 use std::{
-    cmp, collections::{HashMap, HashSet}, hash::{Hash, Hasher}, net::
-    {IpAddr, Ipv4Addr, Ipv6Addr}
+    cmp,
+    cmp::Reverse,
+    collections::{HashMap, HashSet, BinaryHeap},
+    hash::{Hash, Hasher},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr},
+    rc::Rc,
 };
 
 use crate::path_attrs::{
@@ -16,82 +20,76 @@ use crate::path_attrs::{
     NEXT_HOP,
 };
 
+#[derive(Eq, PartialEq, Hash)]
 pub (crate) enum RouteSource {
     Ebgp,
     Ibgp
 }
 
-// Used to hold information related to running the decision process for a particular path
-// The best way to store paths would be using
-// a BTreeSet, ordered based on some preference function, based on the given decision process terms.
-pub(crate) struct RouteAttribute {
-    peer_id: IpAddr, 
-    peer_address: IpAddr, 
-    route_source: RouteSource, 
-    path_attrs: Vec<PathAttr>,
-    best_path: bool,
-    igp_metric: u64,
-}
-impl RouteAttribute {
-    pub fn new(peer_id: IpAddr,
-               peer_address: IpAddr,
-               route_source: RouteSource,
-               path_attrs: Vec<PathAttr>) -> Self {
-        Self {
-            peer_id,
-            peer_address,
-            route_source,
-            path_attrs,
-            best_path: false,
-            igp_metric: 0
+impl From<&RouteSource> for u8 {
+    fn from(value: &RouteSource) -> Self {
+        match value {
+            RouteSource::Ebgp => 0,
+            RouteSource::Ibgp => 1
         }
     }
-    pub fn route_source(&self) -> &RouteSource {
-        &self.route_source
-    }
-    pub fn path_attrs(&self) -> &Vec<PathAttr> {
-        &self.path_attrs
-    }
 }
+
 // This data structure is used to simplify comparisons between many candidate paths
 // to a destination as opposed to destructuring the raw path attribute data for each comparison.
-
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Hash)]
 pub(crate) struct DecisionProcessData {
     local_pref: Option<u32>,
     as_path_len: u8,
+    last_as: u16,
     origin: u8,
     med: u32,
-    route_souce: u8,
+    route_souce: RouteSource,
     igp_cost: u64,
-    peer_id: u32,
-    // Deciding to use IpAddr here since cmp is already defined
+    peer_id: Ipv4Addr,
     peer_addr: IpAddr
+}
+
+impl DecisionProcessData {
+    pub fn new() -> Self { todo!()}
 }
 
 // Implementing PartialOrd (and Ord, implicitly) for this data structure will be critical in
 // allowing the best paths to easily be found and for feasible paths to always
-// be ordered (using min heaps per destination).
+// be ordered (using min heaps per destination). Paths that evaluate to "less than"
+// are better paths.
 impl PartialOrd for DecisionProcessData {
-    fn partial_cmp(&self, other: &Self) ->Option<std::cmp::Ordering> {
-        // First check to see if LOCAL_PREF can be compared
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        // First check to see if local pref can be compared
        let lp_ord = match (self.local_pref, other.local_pref) {
             (Some(left), Some(right)) => {
-                // If so, compare local_pref and return Option
-                // with the result.
-                Some(left.cmp(&right))
+                // If so, compare local pref and return Option
+                // with the result. Note that the order for cmp is switched!
+                // We want to prefer higher local preference, but make that evaluate
+                // to "less than".
+                Some(right.cmp(&left))
             },
             (None, _) | (_, None) => { None }
         };
         // Define a closure that does the non local pref comparisons
         let f = || {
-            self.as_path_len.cmp(&other.as_path_len)
-            .then_with(|| self.origin.cmp(&other.origin))
-            .then_with(|| self.med.cmp(&other.med))
-            .then_with(|| self.route_souce.cmp(&other.route_souce))
-            .then_with(|| self.igp_cost.cmp(&other.igp_cost))
-            .then_with(|| self.peer_id.cmp(&other.peer_id))
-            .then_with(|| self.peer_addr.cmp(&other.peer_addr))
+            let comp = self.as_path_len.cmp(&other.as_path_len) // Shortest AS path wins
+            .then(self.origin.cmp(&other.origin)); // Lowest origin wins
+
+            // Before comparing med, need to verify both paths have same last_as
+            // lowest med wins.
+            let comp = if self.last_as == other.last_as {
+                comp.then(self.med.cmp(&other.med))
+            } else {
+                comp
+            };
+            // Continue comparions
+            let this_rs: u8 = (&self.route_souce).into();
+            let other_rs: u8 = (&other.route_souce).into();
+            comp.then(this_rs.cmp(&other_rs)) // lowest source wins (based on defined From)
+            .then(self.igp_cost.cmp(&other.igp_cost)) // Lowest IGP cost wins
+            .then(self.peer_id.cmp(&other.peer_id)) // Lowest peer id wins
+            .then(self.peer_addr.cmp(&other.peer_addr)) // Lowest peer addr wins
         };
         // Now can check the result and continue the comparison if necessary
         match lp_ord {
@@ -112,7 +110,6 @@ impl Ord for DecisionProcessData {
     fn cmp(&self, other: &Self) -> cmp::Ordering {
         self.partial_cmp(other).unwrap()
     }
-    
 }
 
 // This is an entry in the Path Attribute Table. The goal is to have a data structure that contains
@@ -120,91 +117,122 @@ impl Ord for DecisionProcessData {
 // parameters necessary for running the Decision process. This implies some data duplication, but since some PAs
 // aren't relevant for the Decision Process (and one structue can be pointed to by many routes), this seems
 // reasonable.
+#[derive(PartialEq, Eq)]
 pub(crate) struct PathAttributeTableEntry {
-    entry: Vec<(DecisionProcessData, Vec<PathAttr>)>
+    decision_data: DecisionProcessData, 
+    raw_path_attrs: Vec<PathAttr>
 }
 
+// Want the Entry to be behind an Rc so that when no paths are pointing to it,
+// it can be cleaned out of the table.
 pub(crate) struct PathAttributeTable {
-    table: HashSet<PathAttributeTableEntry>
+    table: HashSet<Rc<PathAttributeTableEntry>>
 }
 
+// The BinaryHeap with reverse effectively makes it a min. heap. Want the paths to be sorted based
+// on their Ordering. The best path evaluates to "less than"
 pub(crate) struct BgpTableEntry<'a> {
-    route_attrs: Vec<RouteAttribute>,
+    paths: BinaryHeap<Reverse<&'a PathAttributeTableEntry>>,
     changed: bool,
-    best: Option<&'a RouteAttribute>
 }
 impl BgpTableEntry<'_> {
     pub fn new() -> Self {
-        Self { route_attrs: Vec::new(), changed: false, best: None }
-    }
-    // Runs the decision process given a new path
-    // and appends the path to the existing list
-    pub fn decision(&mut self, new_path: RouteAttribute) {
-        // Add new path to the table entry
-        self.route_attrs.push(new_path);
-        for path in self.route_attrs.iter() {
-            // Check whether route is internal or external
-            match path.route_source() {
-                RouteSource::Ibgp => {
-                    todo!()
-                },
-                RouteSource::Ebgp => {
-                    todo!()
-                }
-            }
-        }
-    }
-    fn compare_local_pref(&mut self, candidate_path: &RouteAttribute) -> Option<&RouteAttribute> {
-        // First define closure to extract the LOCAL_PREF value from each path. This function
-        // assumes the LOCAL_PREF PA exists in all paths of the table entry.
-        let f = |r: &RouteAttribute| {
-            let lp: Option<&PathAttr> = r.path_attrs()
-            .iter()
-            .filter(|pa| {
-                pa.attr_type_code() == LOCAL_PREF
-            })
-            .last();
-            
-            // Return the LOCAL_PREF value after casting to u32
-            if let Some(pa) = lp {
-                let mut buf: [u8; 4] = [0u8; 4];
-                buf.copy_from_slice(pa.attr_value());
-                u32::from_be_bytes(buf)
-            } else { // This branch implies that None was returned from the iter adaptor and there is an issue
-                0
-            }
-        };
-
-        // Now we can map over all the paths to find the one with maximum LP and compare it to the new candidate path
-        // First need to add the new candidate path to the table.
         todo!()
-        
     }
-    fn compare_as_path(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_origin(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_med(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_route_source(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_igp_metric(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_peer_id(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-    fn compare_peer_addr(&self, other:&RouteAttribute) -> Option<&RouteAttribute> {
-        todo!();
-    }
-
 }
 
 // Will be generic over AFI (v4/v6)
 struct BgpTable<'a, A> {
     table: HashMap<A, BgpTableEntry<'a>>
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decision_data_cmp_lp() {
+        let ip_addr = Ipv4Addr::new(192, 168, 1, 1);
+        let best = DecisionProcessData {
+            local_pref: Some(1000),
+            as_path_len: 0,
+            last_as: 0,
+            origin: 0,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+        let candidate = DecisionProcessData {
+            local_pref: Some(100),
+            as_path_len: 0,
+            last_as: 0,
+            origin: 0,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+
+        assert!(candidate > best);
+    }
+    #[test]
+    fn decision_data_cmp_as_path_len() {
+        let ip_addr = Ipv4Addr::new(192, 168, 1, 1);
+        let best = DecisionProcessData {
+            local_pref: Some(1000),
+            as_path_len: 5,
+            last_as: 0,
+            origin: 0,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+        let candidate = DecisionProcessData {
+            local_pref: Some(1000),
+            as_path_len: 10,
+            last_as: 0,
+            origin: 0,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+
+        assert!(candidate > best);
+    }
+    #[test]
+    fn decision_data_cmp_origin() {
+        let ip_addr = Ipv4Addr::new(192, 168, 1, 1);
+        let best = DecisionProcessData {
+            local_pref: Some(1000),
+            as_path_len: 0,
+            last_as: 0,
+            origin: 0,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+        let candidate = DecisionProcessData {
+            local_pref: Some(1000),
+            as_path_len: 10,
+            last_as: 0,
+            origin: 1,
+            med: 0,
+            route_souce: RouteSource::Ibgp,
+            igp_cost: 0,
+            peer_id: ip_addr.clone(),
+            peer_addr: IpAddr::V4(ip_addr.clone())
+        };
+
+        assert!(candidate > best);
+    }
 }
